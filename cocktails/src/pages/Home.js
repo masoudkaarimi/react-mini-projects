18be3404368dbd80d08bbee6fd4185ff490a21b7
Masoud <masoudk1466@gmail.com>
2022-03-17 21:22:45 +0330

    Pages adding

---
+++ cocktails/src/pages/Home.js
@@ -0,0 +1,11 @@
+import CocktailList from '../components/CocktailList'
+import SearchForm from '../components/SearchForm'
+
+export default function Home() {
+    return (
+        <main>
+            <SearchForm/>
+            <CocktailList/>
+        </main>
+    )
+}
