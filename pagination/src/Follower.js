2684dbfda014bf134a35c9d3a4f51a7e871acc78
Masoud <masoudk1466@gmail.com>
2022-03-22 10:31:23 +0430

    Pagination added

---
+++ pagination/src/Follower.js
@@ -0,0 +1,13 @@
+const Follower = ({ avatar_url, html_url, login }) => {
+  return (
+    <article className='card'>
+      <img src={avatar_url} alt={login} />
+      <h4>${login}</h4>
+      <a href={html_url} className='btn'>
+        view profile
+      </a>
+    </article>
+  )
+}
+
+export default Follower
