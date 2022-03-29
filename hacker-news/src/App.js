2075fe6ab32559a61ef10d7b4f0729b2a6bb7040
Masoud <masoudk1466@gmail.com>
2022-03-29 13:23:33 +0430

    Hacker news finished

---
+++ hacker-news/src/App.js
@@ -0,0 +1,15 @@
+import SearchForm from './SearchForm';
+import Stories from './Stories';
+import Buttons from './Buttons';
+
+function App() {
+	return (
+		<>
+			<SearchForm />
+			<Buttons />
+			<Stories />
+		</>
+	);
+}
+
+export default App;
