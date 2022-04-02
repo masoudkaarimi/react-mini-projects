d370f16b97f233e5fb8c39fb9964e2991826a3dd
Masoud <masoudk1466@gmail.com>
2022-04-02 13:29:14 +0430

    Quiz finished

--- quiz/src/context.js
+++ quiz/src/context.js
@@ -9,8 +9,6 @@ const table = {
 
 const API_ENDPOINT = 'https://opentdb.com/api.php?';
 
-const url = '';
-const tempUrl = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
